69cb822f9cc32b2634adcb1081c4c8390c532e0f
Paul Fayoux <paul.fayoux@gmail.com>
2021-02-01 20:25:57 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
-emcc -o spaghetti.html spaghetti.c -O3 -s WASM=1 -s NO_EXIT_RUNTIME=1 \
+emcc -o counting.html counting.c -O3 -s WASM=1 -s NO_EXIT_RUNTIME=1 \
 -s "EXTRA_EXPORTED_RUNTIME_METHODS=['ccall', 'cwrap', 'writeArrayToMemory', 'getValue']" \
--s EXPORTED_FUNCTIONS="['_malloc', '_free', '_max', '_spaghettiSort']"
+-s EXPORTED_FUNCTIONS="['_malloc', '_free', '_max', '_countingSort']"
